c5141a2576b040d14f693804b2572141cc941d7c
Simon Kim <spamhere@kaoma.net>
2016-09-24 22:32:14 +0900

    Add AVCaptureSample in a workspace

---
+++ AVCaptureSample/AVCaptureSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVCaptureSample
+//
+//  Created by Simon Kim on 24/09/2016.
+//  Copyright © 2016 DZPub.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AVCaptureSample/AVCaptureSampleTests/AVCaptureSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  AVCaptureSampleTests.swift
+//  AVCaptureSampleTests
+//
+//  Created by Simon Kim on 24/09/2016.
+//  Copyright © 2016 DZPub.com. All rights reserved.
+//
+
+import XCTest
+@testable import AVCaptureSample
+
+class AVCaptureSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
